eb4470b70fe77999a5a288503086fb200097afd1
0r080r0 <drumscalation@gmail.com>
2025-07-09 22:42:20 +0200

    fu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://0r080r0.github.io',
   base: '/', // change this to your repo name if it's not your main github.io repo
   output: 'static',
-  vite: {
-    plugins: [tailwindcss()]
-  },
+  integrations: [tailwind()],
   build: {
     assets: 'assets'
   }
