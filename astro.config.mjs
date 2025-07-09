a955df0f9a87d5035eaa5db4a0f1fb8cc943ee34
0r080r0 <drumscalation@gmail.com>
2025-07-09 22:59:43 +0200

    Switch to stable Astro Tailwind v3 setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://0r080r0.github.io',
-  base: '/', // change this to your repo name if it's not your main github.io repo
+  base: '/',
   output: 'static',
   integrations: [tailwind()],
   build: {
